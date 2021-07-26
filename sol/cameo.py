ac9c045470169d4d501955e79d895f374e37c8bc
Diego Valencia <dvr.diegoz@gmail.com>
2021-07-25 21:12:49 -0400

    update sol

--- sol/cameo.py
+++ sol/cameo.py
@@ -1,9 +1,9 @@
-cameo = input().lower().replace(" ","")
-frase = input().lower()
+
 def buscarPalabra(word,para):
     lenword = len(word)
     i = 0
     j=0
+    #print("washoooo")
     while j < len(para):
         if word[i]==para[j]:
             i = i+1
@@ -11,16 +11,26 @@ def buscarPalabra(word,para):
         if i==lenword:
             return j
     return -1
-k = 0
-matches=0
-encontrada = -1
-while k < len(frase):
-    if cameo[0]==frase[k]:
-        encontrada = buscarPalabra(cameo,frase[k:])
-    if encontrada == -1:
-        k=k+1
-    else:
-        matches = matches +1
-        k = k+encontrada
-print(matches)
+def mainProgram():
+    cameo = input().lower().replace(" ","")
+    frase = input().lower()
+    k = 0
+    matches=0
+    encontrada = -1
+    if len(cameo)>len(frase):
+        return 0
+    while k < len(frase):
+        #print(cameo[0],frase[k])
+        if cameo[0]==frase[k]:
+            encontrada = buscarPalabra(cameo,frase[k:])
+        else:
+            encontrada=-1
+        #print(encontrada,k,len(frase),frase[k:])
+        if encontrada == -1:
+            k=k+1
+        else:
+            matches = matches +1
+            k = k+encontrada
+    return matches
     
+print(mainProgram())
